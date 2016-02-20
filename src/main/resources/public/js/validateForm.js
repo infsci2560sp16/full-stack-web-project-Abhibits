5d798ff12f10d1c4cf543eb6330202f5b2505957
Abhibits <abhimukh91@gmail.com>
2016-02-19 20:38:42 -0500

    x

--- src/main/resources/public/js/validateForm.js
+++ src/main/resources/public/js/validateForm.js
@@ -1,8 +1,67 @@
 
- function checkPassword(str)
-  {
-    // at least one number, one lowercase and one uppercase letter
-    // at least six characters that are letters, numbers or the underscore
-    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
-    return re.test(str);
-  }
+function employeeValidation()
+{
+ var firstname= document.getElementById("cust_name").value;
+
+ var email= document.getElementById("cust_email").value;
+ var street= document.getElementById("street").value;
+ var city= document.getElementById("city").value;
+ var state= document.getElementById("state").value;
+
+ var zipcode= parseInt(document.getElementById("zipcode").value);
+
+ var pass=document.getElementById("pass").value;
+ var pass1=document.getElementById("pass1").value;
+ var emailreg= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[a-zA-Z]{2,6}$/;
+ var alphareg=/^[A-Za-z]+$/;
+ var alphanum= /^[0-9a-zA-Z]+$/;
+
+
+
+ if(firstname!=="" && street!=="" && city!=="" && state!=="" && zipcode!=="" && email!=="")
+ {
+   if(email.match(emailreg))
+   {
+     if(firstname.match(alphareg))
+     {
+       if(employeeid.match(alphanum))
+       {
+               if(pass.match(pass1))
+         {
+           if(pass.length>=6 && pass.length<=20)
+           {
+           alert("data suceessfully validated");
+                 return true;
+           }
+           else
+           {
+           alert("password must contain 6 to 20 character");
+                      return false;
+           }
+         }
+         else
+         {
+         alert("password cannot be matched");
+         return false;
+         }
+
+       }
+
+     else{
+       alert("enter a valid first name");
+       return false;
+     }
+   }
+
+   else{
+     alert("enter a valid email");
+     return false;
+   }
+ }
+ else{
+   alert("all fields are required");
+   return false;
+ }
+
+}
+}
